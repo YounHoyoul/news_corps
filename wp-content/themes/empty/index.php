2a38b4721ddb3f86d0c8a2fbd5def34b84454457
Luis Youn <yhy0215@gmail.com>
2020-11-10 02:22:39 +1100

    add posts

--- wp-content/themes/empty/index.php
+++ wp-content/themes/empty/index.php
@@ -23,6 +23,37 @@
     <div id="form_app"></div>
   </div>
 
+  <div class="container my-5">
+    <div class="row">
+        <div class="blog-posts col-md-8">
+        <?php if ( have_posts() ): ?>
+            <?php while( have_posts() ): ?>
+                <?php the_post(); ?>
+                <div class="blog-post my-4">
+                    <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                    <?php if ( has_post_thumbnail() ) :
+                        $featured_image = wp_get_attachment_image_src( get_post_thumbnail_id( get_the_ID() ), 'medium' ); ?>
+                        <div class="blog-post-thumb">
+                            <a href="<?php the_permalink(); ?>"><img src="<?php echo $featured_image[0]; ?>" alt='' /></a>
+                        </div>
+                    <?php endif; ?>
+                    <?php the_excerpt(); ?>
+                    <a class="read-more-link" href="<?php the_permalink(); ?>"><?php _e( 'Read More' ); ?></a>
+                    <div class="posted-in">
+                        <span><?php _e( 'Posted In', 'nd_dosth' ); ?></span>
+                        <span><?php the_category( ', ' ); ?></span>
+                    </div>
+                </div>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <p><?php _e( 'No Blog Posts found', 'nd_dosth' ); ?></p>
+        <?php endif; ?>
+        </div>
+        <div id="blog-sidebar" class="col-md-4">
+        </div>
+    </div>
+  </div>
+
   <?php get_footer(); ?>
 
   <?php wp_footer(); ?>
